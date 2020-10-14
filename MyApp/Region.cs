db4e45f6ddd3b5c7e07fba06cdcf80a9c2488a9f
Casser <paco.hanafi@epsi.fr>
2020-10-14 11:59:47 +0200

    Début exo5

---
+++ MyApp/Region.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyApp
+{
+    public class Region
+    {
+        public string nom;
+        public int code;
+    }
+}
---
+++ MyApp/Services/RegionService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyApp.Services
+{
+    public class RegionService
+    {
+        private DemandeALutilisateur demandeALutilisateur;
+
+    }
+}
