71f706777eec894afd801ebc021db2f8256624e6
Casser <paco.hanafi@epsi.fr>
2020-10-14 08:54:52 +0200

    Ajoutez des fichiers projet.

---
+++ MyApp/Commune.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyApp
+{
+   public class Commune
+    {
+        public string Nom;
+        public int CodePost;
+        public int NbHab;
+    }
+}
